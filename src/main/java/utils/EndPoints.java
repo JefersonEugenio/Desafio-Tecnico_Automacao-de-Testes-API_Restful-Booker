01fc3316fd7315d8f59a35466deacfdf3a40e5e0
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2026-02-03 16:04:23 -0300

    feat: adiciona endPoint autenticacao

--- src/main/java/utils/EndPoints.java
+++ src/main/java/utils/EndPoints.java
@@ -3,5 +3,7 @@ package utils;
 public class EndPoints {
 
     protected static final String BASE_URI_BOOKER = "https://restful-booker.herokuapp.com";
+    protected static final String AUTH = "/auth";
+
 
 }
