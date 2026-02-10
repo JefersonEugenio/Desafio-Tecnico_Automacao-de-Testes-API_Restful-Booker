84cbda1297651da038f4d3e5b134a233499a5de1
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2026-02-10 12:42:32 -0300

    refactor: extrair RequestSpecBuilder para RequestSpecFactory

--- src/test/java/baseTests/BaseTest.java
+++ src/test/java/baseTests/BaseTest.java
@@ -1,20 +1,18 @@
 package baseTests;
 
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeEach;
+import specs.RequestSpecFactory;
 import utils.EndPoints;
 
 public class BaseTest extends EndPoints {
 
-    public static RequestSpecification requestSpec;
+    protected RequestSpecification requestSpec;
 
     @BeforeEach
     public void setUp() {
 
-        requestSpec = new RequestSpecBuilder()
-                .setBaseUri(BASE_URI_BOOKER)
-                .build();
+        requestSpec = RequestSpecFactory.get();
 
     }
 
