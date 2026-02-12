309796516944b4cd4feb3478129daf083322a4de
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2026-02-12 11:51:32 -0300

    test: adicionar dois metodos (id inexistente e sem token) para teste delete

--- src/test/java/restfulBookerTests/reserva/ReservaDeleteTest.java
+++ src/test/java/restfulBookerTests/reserva/ReservaDeleteTest.java
@@ -26,4 +26,35 @@ public class ReservaDeleteTest extends BaseTest {
         ;
     }
 
+    @Test
+    public void reserva_DeleteIdInExistente_RetornarComStatus404() {
+
+        String token = Token.autenticar_CriarEObterToken();
+
+        RestAssured.given()
+                .spec(requestSpec)
+                .cookie("token", token)
+            .when()
+                .delete(BOOKING+"/999999")
+            .then()
+                .log().body()
+                .statusCode(405)
+                .body(equalTo("Method Not Allowed"))
+        ;
+    }
+
+    @Test
+    public void reserva_DeleteIdSemToken_RetornarComStatus403() {
+
+        RestAssured.given()
+                .spec(requestSpec)
+            .when()
+                .delete(BOOKING+"/1")
+            .then()
+                .log().body()
+                .statusCode(403)
+                .body(equalTo("Forbidden"))
+        ;
+    }
+
 }
